c58dceb62aa1c40801644f42a96e96c4177ab99a
Andreu-G <andreugonzalezm@gmail.com>
2024-09-28 12:50:04 +0200

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,7 @@
 /*
     We are using constants instead of env due to pm2 not loading env correctly in Raspberry Pi. Pull requests to fix this issue are most welcome.
 */
+export const REQUEST_SECRET = '12345678';
 export const PORT = 3000;
 export const CHANNELS_TOTAL = 4;
-export const CHANNELS_PINS = [7, 3, 22, 25];
+export const CHANNELS_PINS = [4, 6, 22, 26];
